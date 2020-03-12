941d6fa20eff4230808e3576f1f7832de437ed60
Petar Peychev <petarpeychev98@gmail.com>
2020-03-12 04:08:04 +0000

    Added initial netbeans ant project

---
+++ src/user/client/UserClient.java
@@ -0,0 +1,15 @@
+package user.client;
+
+/**
+ * Provides a user interface for monitoring weather station data. 
+ */
+public class UserClient {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
